972de34426f38a743c7783db2a258e07b4858146
David Keeler <dkeeler@mozilla.com>
2014-11-11 15:12:13 -0800

    fixes #1: picking which certificate to use works

--- data/certChooser.js
+++ data/certChooser.js
@@ -1,31 +1,30 @@
-var args = window.arguments.slice();
-var domain = args.shift();
+// data is an object with the following properties:
+//   domain: a string representing the domain that called signText
+//   textToSign: the text being signed
+//   certs: a map of nicknames to objects with the following properties:
+//     nickname: the nickname of a certificate
+//     subject: the subject of a certificate
+//     issuer: the issuer of a certificate
+//     token: the token the certificate lives on
+//   cancelled: to be set to true if the user cancels the dialog
+//   selectedCert: the nickname of the selected certificate
+var data = window.arguments[0];
 var headline = document.getElementById("headline");
-headline.textContent = headline.textContent.replace("<site here>", domain);
-var textToSign = args.shift();
+headline.textContent = headline.textContent.replace("<site here>", data.domain);
 var textArea = document.getElementById("text");
-textArea.textContent = textToSign;
+textArea.textContent = data.textToSign;
 var certNicknames = document.getElementById("certNicknames");
-var certs = {};
-while (args.length > 0) {
+for (var nickname in data.certs) {
   var option = document.createElement("option");
-  var nickname = args.shift();
   option.setAttribute("value", nickname);
   option.textContent = nickname;
   certNicknames.appendChild(option);
-  var subject = args.shift();
-  var issuer = args.shift();
-  var token = args.shift();
-  certs[nickname] = {
-    subject: subject,
-    issuer: issuer,
-    token: token
-  };
 }
 
 function displayCertDetails() {
   var selection = document.getElementById("certNicknames");
-  var certDetails = certs[selection.value];
+  data.selectedCert = selection.value;
+  var certDetails = data.certs[selection.value];
   var detailsElement = document.getElementById("certDetails");
   detailsElement.textContent = "Issued to: " + certDetails.subject + "\n" +
                                "Issued by: " + certDetails.issuer + "\n" +
@@ -33,3 +32,13 @@ function displayCertDetails() {
 }
 
 displayCertDetails();
+
+function doOK() {
+  data.cancelled = false;
+  window.close();
+}
+
+function doCancel() {
+  data.cancelled = true;
+  window.close();
+}
